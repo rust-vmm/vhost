96c31bbbc6150685b425e403434d8bc1c2173954
Liu Jiang <gerry@linux.alibaba.com>
2021-02-19 22:28:52 +0800

    vhost_kern/vsock: implemnt VhostVsock
    Refine VhostVsock trait definition, and implement it for kernel
    based vsock driver.
    
    Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>

--- src/vhost_kern/vsock.rs
+++ src/vhost_kern/vsock.rs
@@ -17,6 +17,7 @@ use vmm_sys_util::ioctl::ioctl_with_ref;
 
 use super::vhost_binding::{VHOST_VSOCK_SET_GUEST_CID, VHOST_VSOCK_SET_RUNNING};
 use super::{ioctl_result, Error, Result, VhostKernBackend};
+use crate::vsock::VhostVsock;
 
 const VHOST_PATH: &str = "/dev/vhost-vsock";
 
@@ -40,30 +41,25 @@ impl<AS: GuestAddressSpace> Vsock<AS> {
         })
     }
 
-    /// Set the CID for the guest.  This number is used for routing all data destined for
-    /// running in the guest. Each guest on a hypervisor must have an unique CID
-    ///
-    /// # Arguments
-    /// * `cid` - CID to assign to the guest
-    pub fn set_guest_cid(&self, cid: u64) -> Result<()> {
-        let ret = unsafe { ioctl_with_ref(&self.fd, VHOST_VSOCK_SET_GUEST_CID(), &cid) };
-        ioctl_result(ret, ())
-    }
-
-    /// Tell the VHOST driver to start performing data transfer.
-    pub fn start(&self) -> Result<()> {
-        self.set_running(true)
-    }
-
-    /// Tell the VHOST driver to stop performing data transfer.
-    pub fn stop(&self) -> Result<()> {
-        self.set_running(false)
-    }
-
     fn set_running(&self, running: bool) -> Result<()> {
         let on: ::std::os::raw::c_int = if running { 1 } else { 0 };
         let ret = unsafe { ioctl_with_ref(&self.fd, VHOST_VSOCK_SET_RUNNING(), &on) };
         ioctl_result(ret, ())
+    }
+}
+
+impl<AS: GuestAddressSpace> VhostVsock for Vsock<AS> {
+    fn set_guest_cid(&self, cid: u64) -> Result<()> {
+        let ret = unsafe { ioctl_with_ref(&self.fd, VHOST_VSOCK_SET_GUEST_CID(), &cid) };
+        ioctl_result(ret, ())
+    }
+
+    fn start(&self) -> Result<()> {
+        self.set_running(true)
+    }
+
+    fn stop(&self) -> Result<()> {
+        self.set_running(false)
     }
 }
 
